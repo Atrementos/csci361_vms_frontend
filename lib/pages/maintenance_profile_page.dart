f9e4164792113cde1649f240f8c23eb2d2b1b33b
deqnqe <diana.ulaskhanova01@gmail.com>
2023-11-19 10:45:19 +0600

    Maintenance profile

---
+++ lib/pages/maintenance_profile_page.dart
@@ -0,0 +1,213 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:csci361_vms_frontend/pages/driver_page.dart';
+import 'package:csci361_vms_frontend/pages/fueling_person_page.dart';
+import 'package:csci361_vms_frontend/pages/maintenance_person_page.dart';
+import 'package:csci361_vms_frontend/providers/jwt_token_provider.dart';
+import 'package:csci361_vms_frontend/providers/page_provider.dart';
+import 'package:csci361_vms_frontend/widgets/admin_drawer.dart';
+import 'package:csci361_vms_frontend/widgets/maintenance_drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+
+class MaintenanceProfilePage extends ConsumerStatefulWidget {
+  const MaintenanceProfilePage({super.key});
+
+  @override
+  ConsumerState<MaintenanceProfilePage> createState() => _MaintenanceProfilePageState();
+}
+
+class  _MaintenanceProfilePageState extends ConsumerState<MaintenanceProfilePage> {
+  Map<String, dynamic>? _userInfo;
+  final _formKey = GlobalKey<FormState>();
+  bool editMode = false;
+  String firstName = '';
+  String lastName = '';
+  String middleName = '';
+  String contactNumber = '';
+  String address = '';
+  String email = '';
+
+  void _loadUser() async {
+    final url = Uri.parse('http://vms-api.madi-wka.xyz/user/me');
+    final response = await http.get(url, headers: {
+      HttpHeaders.authorizationHeader:
+      'Bearer ${ref.read(jwt.jwtTokenProvider)}'
+    });
+    Map<String, dynamic> decodedResponse = json.decode(response.body);
+    setState(() {
+      _userInfo = decodedResponse;
+      print(_userInfo);
+      firstName = _userInfo!['Name'];
+      lastName = _userInfo!['LastName'];
+      if (_userInfo!['MiddleName'] != null) {
+        middleName = _userInfo!['MiddleName'];
+      }
+      contactNumber = _userInfo!['ContactNumber'];
+      address = _userInfo!['Address'];
+      email = _userInfo!['Email'];
+    });
+  }
+
+  @override
+  void initState() {
+    _loadUser();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget mainContent;
+    if (_userInfo == null) {
+      mainContent = const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      mainContent = Container(
+        alignment: Alignment.topCenter,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Form(
+                key: _formKey,
+                child: Expanded(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextFormField(
+                              readOnly: editMode ? false : true,
+                              initialValue: firstName,
+                              decoration: const InputDecoration(
+                                label: Text('First Name'),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          Expanded(
+                            child: TextFormField(
+                              readOnly: editMode ? false : true,
+                              initialValue: lastName,
+                              decoration: const InputDecoration(
+                                label: Text('Last Name'),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextFormField(
+                              readOnly: editMode ? false : true,
+                              initialValue: contactNumber,
+                              decoration: const InputDecoration(
+                                label: Text('Contact Number'),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          Expanded(
+                            child: TextFormField(
+                              readOnly: editMode ? false : true,
+                              initialValue: middleName,
+                              decoration: const InputDecoration(
+                                label: Text('Middle Name'),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextFormField(
+                              readOnly: editMode ? false : true,
+                              initialValue: address,
+                              decoration: const InputDecoration(
+                                label: Text('Address'),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          Expanded(
+                            child: TextFormField(
+                              readOnly: editMode ? false : true,
+                              initialValue: email,
+                              decoration: const InputDecoration(
+                                label: Text('Email'),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      if (editMode)
+                        const SizedBox(
+                          height: 12,
+                        ),
+                      if (editMode)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  editMode = false;
+                                  _formKey.currentState!.reset();
+                                });
+                              },
+                              child: const Text('Cancel'),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: const Text('Save'),
+                            ),
+                          ],
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+        actions: [
+          if (_userInfo == null || !editMode)
+            TextButton.icon(
+              onPressed: () {
+                setState(() {
+                  editMode = !editMode;
+                });
+              },
+              icon: const Icon(Icons.edit),
+              label: const Text('Edit'),
+            ),
+        ],
+      ),
+      body: mainContent,
+      drawer: const MaintenanceDrawer(),
+    );
+  }
+}
---
+++ lib/pages/vehicle_for_maintenance_page.dart
@@ -0,0 +1,75 @@
+import 'package:csci361_vms_frontend/models/vehicle.dart';
+import 'package:csci361_vms_frontend/pages/map_page.dart';
+import 'package:csci361_vms_frontend/widgets/admin_drawer.dart';
+import 'package:csci361_vms_frontend/widgets/maintenance_drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:csci361_vms_frontend/data/dummy_vehicles.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class VehiclesForMaintenancePage extends ConsumerWidget {
+  VehiclesForMaintenancePage({super.key});
+
+  final List<Vehicle> _registeredVehicles = dummyVehicles;
+  final _formKey = GlobalKey<FormState>();
+
+  void _addVehicle() {
+    // TODO (Validate->Save->Sync with backend)
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Vehicles Page'),
+      ),
+      body: Column(
+        children: [
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (ctx) => const MapPage(),
+                    ),
+                  );
+                },
+                child: const Text('View All Vehicles'),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: _registeredVehicles.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Text(
+                    _registeredVehicles[index].licensePlate,
+                  ),
+                  title: Text(
+                    ' ${_registeredVehicles[index].model}, ${_registeredVehicles[index].year}',
+                  ),
+                  trailing: const Icon(Icons.arrow_outward),
+                );
+                // const Row(
+                //   children: [
+                //     Text('Vehicle license plate'),
+                //     Text('Vehicle model'),
+                //     Text('Vehicle year')
+                //   ],
+                // );
+              },
+            ),
+          ),
+        ],
+      ),
+      drawer: const MaintenanceDrawer(),
+    );
+  }
+}
